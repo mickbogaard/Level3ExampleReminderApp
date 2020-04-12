c0aa5658ed45a11107155f5e48ae31d371bb20f3
Mick Bogaard <mick.bogaard@hva.nl>
2020-04-12 14:51:04 +0200

    finished...

---
+++ app/src/main/java/com/example/reminderapp/ReminderRepository.kt
@@ -0,0 +1,32 @@
+package com.example.reminderapp
+
+import android.content.Context
+import layout.Reminder
+
+
+public class ReminderRepository(context: Context) {
+
+    private var reminderDao: ReminderDao
+
+    init {
+        val reminderRoomDatabase = ReminderRoomDatabase.getDatabase(context)
+        reminderDao = reminderRoomDatabase!!.reminderDao()
+    }
+
+    suspend fun getAllReminders(): List<Reminder> {
+        return reminderDao.getAllReminders()
+    }
+
+    suspend fun insertReminder(reminder: Reminder) {
+        reminderDao.insertReminder(reminder)
+    }
+
+    suspend fun deleteReminder(reminder: Reminder) {
+        reminderDao.deleteReminder(reminder)
+    }
+
+    suspend fun updateReminder(reminder: Reminder) {
+        reminderDao.updateReminder(reminder)
+    }
+
+}
